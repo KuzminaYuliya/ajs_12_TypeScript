b9813b9cd6d1c4733ec0d695396fff76bc9a346d
KuzminaYuliya <kuzmina_h@mail.ru>
2021-07-29 00:10:53 +0500

    first commit for ajs_12_typescript

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,58 +1,47 @@
 import Cart from '../service/Cart';
 import Movie from '../domain/Movie';
 
-test('new card should be empty', () => {
+test('should add movie', () => {
   const cart = new Cart();
-
-  expect(cart.items.length).toBe(0);
-});
-
-test('add movie', () => {
-  const cart = new Cart();
-  const avengers = new Movie(1020, 'Мстители', 'The Avengers', 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137, 'avengers.png', 500.00);
-  cart.add(avengers);
+  const movieCart = new Movie(1020, 'Мстители', 'The Avengers', 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137, 'avengers.png', 500.00);
+  cart.add(movieCart);
   expect(cart.items.length).toBe(1);
 });
 
-test('total for empty cart', () => {
+test('should return total sum', () => {
   const cart = new Cart();
-  expect(cart.totalSum()).toBe(0);
+  const movieCart = new Movie(1020, 'Мстители', 'The Avengers', 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137, 'avengers.png', 500.00);
+  const movieCart2= new Movie(1021, 'Мстители2', 'The Avengers', 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137, 'avengers.png', 500.00);
+  cart.add(movieCart);
+  cart.add(movieCart2);
+  expect(cart.totalSum()).toBe(1000);
 });
 
-test('check total sum', () => {
+test('should return discount', () => {
   const cart = new Cart();
-  const avengers = new Movie(1020, 'Мстители', 'The Avengers', 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137, 'avengers.png', 500.00);
-  const film = new Movie(1010, 'Lala', 'Lala', 1999, 'UK', 'Lalalal', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 120, 'lalala.png', 400.00);
-  cart.add(avengers);
-  cart.add(film);
-  expect(cart.totalSum()).toBe(900);
+  const movieCart = new Movie(1020, 'Мстители', 'The Avengers', 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137, 'avengers.png', 500.00);
+  const movieCart2= new Movie(1021, 'Мстители2', 'The Avengers', 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137, 'avengers.png', 500.00);
+  cart.add(movieCart);
+  cart.add(movieCart2);
+  expect(cart.finalTotalSum(50)).toBe(500);
 });
 
-test('check total with discount', () => {
+test('should delete 1 item', () => {
   const cart = new Cart();
-  const avengers = new Movie(1020, 'Мстители', 'The Avengers', 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137, 'avengers.png', 500.00);
-  const film = new Movie(1010, 'Lala', 'Lala', 1999, 'UK', 'Lalalal', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 120, 'lalala.png', 400.00);
-  cart.add(avengers);
-  cart.add(film);
-  expect(cart.finalTotalSum(50)).toBe(450);
-});
-
-test('delete item', () => {
-  const cart = new Cart();
-  const avengers = new Movie(1020, 'Мстители', 'The Avengers', 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137, 'avengers.png', 500.00);
-  const film = new Movie(1010, 'Lala', 'Lala', 1999, 'UK', 'Lalalal', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 120, 'lalala.png', 400.00);
-  cart.add(avengers);
-  cart.add(film);
+  const movieCart = new Movie(1020, 'Мстители', 'The Avengers', 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137, 'avengers.png', 500.00);
+  const movieCart2= new Movie(1021, 'Мстители2', 'The Avengers', 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137, 'avengers.png', 500.00);
+  cart.add(movieCart);
+  cart.add(movieCart2);
   cart.delete(1020);
   expect(cart.items.length).toBe(1);
 });
 
-test('delete non-existent item', () => {
+test('should not delete item', () => {
   const cart = new Cart();
-  const avengers = new Movie(1020, 'Мстители', 'The Avengers', 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137, 'avengers.png', 500.00);
-  const film = new Movie(1010, 'Lala', 'Lala', 1999, 'UK', 'Lalalal', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 120, 'lalala.png', 400.00);
-  cart.add(avengers);
-  cart.add(film);
-  cart.delete(555);
+  const movieCart = new Movie(1020, 'Мстители', 'The Avengers', 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137, 'avengers.png', 500.00);
+  const movieCart2= new Movie(1021, 'Мстители2', 'The Avengers', 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137, 'avengers.png', 500.00);
+  cart.add(movieCart);
+  cart.add(movieCart2);
+  cart.delete(1022);
   expect(cart.items.length).toBe(2);
 });
